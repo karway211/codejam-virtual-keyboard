c390e151026a650d04d6a734aeb1f59a7c539510
Andrey K <karway211@gmail.com>
2019-11-02 13:22:49 +0300

    feat: add key names

--- index.js
+++ index.js
@@ -45,6 +45,36 @@ keyboardArr4[0].style.width = '100px';
 keyboardArr4[12].style.width = '85px';
 keyboardArr5[3].style.width = '330px';
 
+let text1 = ['&acute;','1', '2', '3', '4', '5;', '6;', '7;', '8', '9', '0', '-', '=', 'Backspace'];
+let text2 = ['Tab','q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '&#91;', '&#93;', '&#92;', 'DEL'];
+let text3 = ['CapsLock','a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', '&#58;', '&#34;', 'ENTER'];
+let text4 = ['Shift','z', 'x', 'c', 'v', 'b', 'n', 'm', '&#60;', '&#62;', '&#63;', '&#8593;', 'Shift'];
+let text5 = ['Ctrl','Win', 'Alt', '', 'Alt', '&larr;', '&darr;', '&rarr;', 'Ctrl'];
+
+
+// for( let i = 0; i < keyboardArr5.length; i++){
+//     for( let j = 0; j < text5.length; j++){
+//         if(i == j){
+//             (keyboardArr5[i]).innerHTML = text5[j];
+//         }
+//     }
+// }
+
+let keyName = (key, name) => {
+    for( let i = 0; i < key.length; i++){
+        for( let j = 0; j < name.length; j++){
+            if(i == j){
+                (key[i]).innerHTML = name[j];
+            }
+        }
+    }
+}
+
+keyName(keyboardArr1, text1);
+keyName(keyboardArr2, text2);
+keyName(keyboardArr3, text3);
+keyName(keyboardArr4, text4);
+keyName(keyboardArr5, text5);
 
 
 
